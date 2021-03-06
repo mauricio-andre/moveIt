931ffd6401c6dd1a22ad6ac7e6b4c89c836189a5
mauricio-andre <mauricio.r.andre@gmail.com>
2021-03-06 16:18:08 -0300

    Layout botão do twitter

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -2,19 +2,28 @@ import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/LevelUpModal.module.css';
 
+import Twitter from '../../public/icons/twitter.svg';
+
 export function LevelUpModal() {
   const { level, closeLevelUpModal } = useContext(ChallengesContext);
 
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <header>{level}</header>
+        <div className={styles.content}>
+          <header>{level}</header>
 
-        <strong>Parabéns</strong>
-        <p>Você alcançou um novo level.</p>
+          <strong>Parabéns</strong>
+          <p>Você alcançou um novo level.</p>
+        </div>
 
-        <button type="button" onClick={closeLevelUpModal}>
+        <button type="button" className={styles.close} onClick={closeLevelUpModal}>
           <img src="/icons/close.svg" alt="Fechar"/>
+        </button>
+
+        <button type="button" className={styles.shareTwitter}>
+          Compartilhar no Twitter
+          <Twitter />
         </button>
       </div>
     </div>
